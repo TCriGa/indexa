5667029c6e80eed8a238253408f76cb6163ac196
tcriga <thay.c.gayv@outlook.com>
2024-06-03 14:48:47 -0400

    Configurations course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './component/container/container.component';
+import { CabecalhoComponent } from './component/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './component/separador/separador.component';
+import { ContatoComponent } from './component/contato/contato.component';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from './agenda.json'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+  ],
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+  filtrarContatosPorLetraInicial(letra:string) : Contato[] {
+    return this.contatos.filter( contato => {
+      return contato.nome.toLowerCase().startsWith(letra)
+    } )
+  }
 }
