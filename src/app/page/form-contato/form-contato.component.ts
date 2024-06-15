b48796646b856fd17257d7210b6e3f0f4f603932
tcriga <thay.c.gayv@outlook.com>
2024-06-15 14:20:29 -0400

    validations

--- src/app/page/form-contato/form-contato.component.ts
+++ src/app/page/form-contato/form-contato.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../component/container/container.component';
 import { SeparadorComponent } from '../../component/separador/separador.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form-contato',
   standalone: true,
   templateUrl: './form-contato.component.html',
   styleUrl: './form-contato.component.css',
-  imports: [ContainerComponent, SeparadorComponent],
+  imports: [ContainerComponent, SeparadorComponent, ReactiveFormsModule],
 })
-export class FormContatoComponent {}
+export class FormContatoComponent {
+  contatoForm!: FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl(''),
+    });
+  }
+  salvarContato() {
+    if (this.contatoForm.valid) {
+      console.log(this.contatoForm.value);
+    }
+  }
+  cancelar() {
+    console.log('cancelado');
+  }
+}
