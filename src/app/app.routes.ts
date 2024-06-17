4bc1b39d9c986a262eac68c01a77c2ffc5f41085
tcriga <thay.c.gayv@outlook.com>
2024-06-17 13:39:03 -0400

    implement routers

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FormContatoComponent } from './page/form-contato/form-contato.component';
+import { ListaContatosComponent } from './page/lista-contatos/lista-contatos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'forms',
+    component: FormContatoComponent,
+  },
+  {
+    path: 'lista-contatos',
+    component: ListaContatosComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/lista-contatos',
+    pathMatch: 'full',
+  },
+];
